bc46b7bc4821b28cda2cce9ef4ac7785abffbb48
Marco Gulino <marco.gulino@gmail.com>
2008-06-20 13:53:46 +0000

    fix FSF address
    svn path=/trunk/playground/multimedia/mplayerthumbs/; revision=822517

--- src/videopreview.cpp
+++ src/videopreview.cpp
@@ -1,5 +1,5 @@
 /***************************************************************************
-   Copyright (C) 2006
+   Copyright (C) 2006-2008
    by Marco Gulino <marco@kmobiletools.org>
 
    This program is free software; you can redistribute it and/or modify
@@ -15,7 +15,7 @@
    You should have received a copy of the GNU General Public License
    along with this program; if not, write to the
    Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-   Boston, MA 02110-1301, USA.
+    Boston, MA 02110-1301, USA.
  ***************************************************************************/
 
 #include "videopreview.h"
